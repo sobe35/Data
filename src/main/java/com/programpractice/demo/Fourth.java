f5777e36e2ebbf92913d284ee40c8d5a92a4caec
sobe35 <drasool325@gmail.com>
2024-05-15 12:05:51 -0700

    first

---
+++ src/main/java/com/programpractice/demo/Fourth.java
@@ -0,0 +1,16 @@
+package com.programpractice.demo;
+
+public class Fourth {
+    public static void main(String[] args) {
+        int a [] ={3,4,5,6,7,8};
+        int sum=0;
+        for (int i=0;i<a.length;i++){
+            sum = sum+i;
+
+        }
+        System.out.println("sum  " +sum);
+    }
+
+}
+
+
